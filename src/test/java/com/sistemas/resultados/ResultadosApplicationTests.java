deb6bb62929e16d1f894f9c5f544bbd53659eb05
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2022-04-04 16:47:09 -0300

    First Commit

---
+++ src/test/java/com/sistemas/resultados/ResultadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemas.resultados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
